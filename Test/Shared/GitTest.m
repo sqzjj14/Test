1b89aab594887c943742ba674f910f62656c480b
wufan <>
2020-12-16 09:54:10 +0800

    product

---
+++ Test/Shared/GitTest.m
@@ -0,0 +1,8 @@
+//
+//  GitTest.m
+//  Test
+//
+//  Created by wufan on 2020/12/16.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Test/Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by wufan on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
